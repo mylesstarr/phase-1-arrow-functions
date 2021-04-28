b5758c33e95fd9f70b954fccca3f1a452775ca7a
Myles Starr <mylesstarr@gmail.com>
2021-04-28 09:08:12 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(num1="2000", num2="100"){
+    return(parseInt(num1)/parseInt(num2))
+}
+
+const square = (parameter) => parameter**parameter
+
+const add = (num1, num2) => num1+num2
